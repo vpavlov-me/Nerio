10a2cdb6be544d07acc44feb532359ff300f3165
Vladimir <vpavlov.me@gmail.com>
2026-07-09 12:38:34 +0400

    Add Yandex Metrika counter

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { DocsChrome } from "../components/docs-chrome";
 
@@ -12,6 +13,8 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
   variable: "--n-font-geist-mono",
 });
+
+const yandexMetrikaId = "110539538";
 
 export const metadata: Metadata = {
   title: "Nerio",
@@ -29,6 +32,34 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     >
       <body>
         <DocsChrome>{children}</DocsChrome>
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {
+                if (document.scripts[j].src === r) { return; }
+              }
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(${yandexMetrikaId}, "init", {
+              clickmap: true,
+              trackLinks: true,
+              accurateTrackBounce: true,
+              webvisor: true
+            });
+          `}
+        </Script>
+        <noscript>
+          <div>
+            <img
+              src={`https://mc.yandex.ru/watch/${yandexMetrikaId}`}
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
       </body>
     </html>
   );
